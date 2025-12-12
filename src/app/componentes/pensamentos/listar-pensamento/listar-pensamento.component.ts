bd754c47ef4c9e360c77234594ea26a74a58668b
WillSantosNe <williamsantosstella@gmail.com>
2025-12-12 11:32:52 -0300

    feat: connect components to api via observables and router
    - Subscribe to 'listar' method in ListarPensamentoComponent
    - Implement 'criar' method in service using HTTP POST
    - Make 'id' optional in Pensamento interface
    - Implement programmatic navigation on save and cancel actions

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -9,9 +10,15 @@ import { Pensamento } from '../pensamento';
 export class ListarPensamentoComponent implements OnInit {
 
   listaPensamentos : Pensamento[] = [];
-  constructor() { }
+  // Injetando o serviço no construtor
+  constructor(private service : PensamentoService) { }
 
+  // Ciclo de vida do componente que vai rodar ao carregar
   ngOnInit(): void {
+    // Uso de subscribe para consumir o Observable retornado do serviço
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
   }
 
 }
